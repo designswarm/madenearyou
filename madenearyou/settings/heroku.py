ed73fafa4c6ee0cb6ee76c55608c9290c1b3a445
Phil Gyford <phil@gyford.com>
2016-07-04 10:43:52 +0100

    Set ALLOWED_HOSTS for heroku

--- madenearyou/settings/heroku.py
+++ madenearyou/settings/heroku.py
@@ -4,3 +4,4 @@ import dj_database_url
 DATABASES['default'] =  dj_database_url.config()
 DATABASES['default']['ENGINE'] = 'django.contrib.gis.db.backends.postgis'
 
+ALLOWED_HOSTS = [".herokuapp.com",]

a56184a5ffb8c1f7368760e96a62a3ceac961c9f
Phil Gyford <phil@gyford.com>
2016-07-07 10:39:14 +0100

    Rationalise forms

--- producers/forms.py
+++ producers/forms.py
@@ -10,19 +10,11 @@ required_attrs = {'class': 'form-control', 'required': 'required',}
 postcode_attrs = {k:v for k,v in required_attrs.items()}
 postcode_attrs['class'] = postcode_attrs['class'] + ' form-control-postcode'
 
-class FindProducerForm(forms.Form):
-    postcode = forms.CharField(required=True, max_length=8,
-                                widget=forms.TextInput(attrs=postcode_attrs))
+
+class HoneypotFormMixin(forms.Form):
     honeypot = forms.CharField(required=False,
                             label='If you enter anything in this field '\
                                 'your data will be treated as spam')
-
-    def clean_postcode(self):
-        """Check postcode validity, as a format."""
-        value = self.cleaned_data["postcode"]
-        if value:
-            validators.validate_uk_postcode(value)
-        return value.strip()
 
     def clean_honeypot(self):
         """Check that nothing's been entered into the honeypot."""
@@ -32,17 +24,26 @@ class FindProducerForm(forms.Form):
         return value
 
 
-class ProducerForm(forms.ModelForm):
+class FindProducerForm(HoneypotFormMixin, forms.Form):
+    postcode = forms.CharField(required=True, max_length=8,
+                                widget=forms.TextInput(attrs=postcode_attrs))
+
+    def clean_postcode(self):
+        """Check postcode validity, as a format."""
+        value = self.cleaned_data["postcode"]
+        if value:
+            validators.validate_uk_postcode(value)
+        return value.strip()
+
+
+class ProducerForm(HoneypotFormMixin, forms.ModelForm):
     products = forms.ModelMultipleChoiceField(queryset=Product.objects.all())
-    honeypot = forms.CharField(required=False,
-                            label='If you enter anything in this field '\
-                                'your data will be treated as spam')
 
     class Meta:
         model = Producer
         # The order the fields will appear in the form:
         fields = ['postcode', 'products', 'business_name', 'contact_name',
-                'phone', 'email', 'url',  ]
+                'phone', 'email', 'url', ]
 
         # Adding attributes to the form fields.
         widgets = {
@@ -61,13 +62,6 @@ class ProducerForm(forms.ModelForm):
         self.fields['products'].help_text = 'This has to be food you make in the country and do not import'
         self.fields['products'].widget = forms.widgets.CheckboxSelectMultiple()
         self.fields['products'].queryset = Product.objects.all()
-
-    def clean_honeypot(self):
-        """Check that nothing's been entered into the honeypot."""
-        value = self.cleaned_data["honeypot"]
-        if value:
-            raise forms.ValidationError(self.fields["honeypot"].label)
-        return value
 
     def clean_postcode(self):
         value = self.cleaned_data['postcode']

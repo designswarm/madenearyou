cf752cbfec908b7589678342e871572fc97245ab
Phil Gyford <phil@gyford.com>
2016-07-08 12:21:05 +0100

    Add navigation and rationlise page title HTML

--- madenearyou/settings/defaults.py
+++ madenearyou/settings/defaults.py
@@ -50,7 +50,6 @@ MIDDLEWARE_CLASSES = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
-    'django.contrib.flatpages.middleware.FlatpageFallbackMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
--- producers/urls.py
+++ producers/urls.py
@@ -1,12 +1,19 @@
 from django.conf.urls import url
+from django.contrib.flatpages import views as flatpages_views
 from django.views.generic import TemplateView
 
 from . import views
+
 
 urlpatterns = [
     url(r'^$',
         views.HomeView.as_view(),
         name='home'),
+
+    # We add Flatpages here, rather than use the middleware, so that we can
+    # easily link to a Flatpage by name in the templates.
+    url(r'^about/$',
+        flatpages_views.flatpage, {'url': '/about/'}, name='about'),
 
     # So we can test these templates when DEBUG=True.
     url(r'^404/$', TemplateView.as_view(template_name='404.html')),
